8357025cd939a44d051ff3fe701bb808ea2f2230
aheeshnagraj <aheesh@gmail.com>
2022-03-28 21:11:03 -0700

    Form data post to server.js working
    Form data post to server.js working

--- step3/server.js
+++ step3/server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const EthCrypto = require('eth-crypto');
+const { request, response } = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({extended:false}))
 //Middleware - folder to get static assets
 app.use(express.static('app'));
+
+app.use(express.json({limit : '1mb'}));
+
+
 
 //Middleware - folder to get contracts
 app.use(express.static('contracts/compiled'));
 
 app.get('/',(req,res) => {
     res.sendFile(`${__dirname}/app/index.html`);
+});
+
+//Setup this api for collecting data from client side UI i.e. form data.
+app.post('/api',(request,response) => {console.log("*************** SERVER /api ROUTE  START ***********************")
+    console.log("######## THIS IS THE REQUEST ##########",request.body);
+    console.log("*************** SERVER /api ROUTE END ***********************")
 });
 
 app.get('*',(req,res) => {
