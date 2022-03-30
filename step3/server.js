05fea3005cedc864eb31dda26d80eeb4a77017a1
aheeshnagraj <aheesh@gmail.com>
2022-03-29 22:17:09 -0700

    pin json to IPFS via server now working
    pin json to IPFS via server now working.

--- step3/server.js
+++ step3/server.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const EthCrypto = require('eth-crypto');
-const { request, response } = require('express');
+//const express = require('express');
+import express from 'express';
+import fetch from 'node-fetch';
+import pinataSDK from '@pinata/sdk';
+//const pinataSDK = require('@pinata/sdk');
+const pinata = pinataSDK('26689a58bbd3e05207c5','622c0634c379b1139f92dfd22f6dd79dd600df1eb09bec13cb70456a5e3835fa');
+//const fetch = require('node-fetch'); 
+
+//const EthCrypto = require('eth-crypto');
+//const { request, response } = require('express');
 const app = express();
 const PORT = 3000;
 
@@ -20,10 +27,45 @@ app.get('/',(req,res) => {
 });
 
 //Setup this api for collecting data from client side UI i.e. form data.
-app.post('/api',(request,response) => {console.log("*************** SERVER /api ROUTE  START ***********************")
-    console.log("######## THIS IS THE REQUEST ##########",request.body);
-    console.log("*************** SERVER /api ROUTE END ***********************")
-});
+app.post('/api',buildjson,pindata,mint);
+/* => {
+    //Write the function to Build JSON and Generate IPFS hash
+    console.log("After pindata call on SERVER.JS /api route ===> ipfs");
+    // Use the IPFS Hash and web3 contract object to mint token
+
+    // Success message Token minted
+    
+    response.end();
+});*/
+
+function buildjson(req,res,next){
+    console.log("*************** SERVER buildjson ROUTE  START ***********************")
+    console.log("######## THIS IS THE REQUEST ##########",req.body);
+    console.log(`*************** SERVER /api ROUTE END ***********************,
+    ${typeof(req.body)},#### parse items x Recipient Address = ,${req.body.x},
+    ### parse item y message : ${req.body.y},
+    ### parse item z Sender Address : ${req.body.z}`);
+    next();
+}
+
+async function pindata(req,res,next) {
+    console.log("Json for IPFS",req.body);
+   //Calling pinata service
+   await  pinata.pinJSONToIPFS(req.body).then((res) => {
+        console.log("pinJSONToIPFS Success",res);
+    }).catch((err) => {
+        console.log(err);
+    });
+    next();
+    //console.log("REsponse object",res.body);
+}
+
+function mint(req,res) {
+    console.log("Mint Function",req.body);
+} 
+
+// Define Route to view inbox
+
 
 app.get('*',(req,res) => {
     res.status(404);
@@ -33,3 +75,10 @@ app.get('*',(req,res) => {
 app.listen(PORT,() => {
     console.log(`Anon Communicator app running on port ${PORT}`);
 });
+
+
+function URI_Fetch(req,res,next) {
+    console.log("****IN URI_FETCH FUNCTION ******");
+    next();
+
+}
