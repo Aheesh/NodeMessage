7875fbd3ef4a3af2a257815445cf3a221bbbcf7c
aheeshnagraj <aheesh@gmail.com>
2022-05-12 15:39:54 -0700

    Node txns working
    RFC connection fixed.
    Account[0] can mint token with ipfs link.
    Works with ganache.
    Metamask now displays the NFT minted and connected to the message recipient.

--- step3/server.js
+++ step3/server.js
@@ -73,29 +73,86 @@ async function pindata(req,res,next) {
 async function mint(req,res) {
     console.log("Mint Function",req.body);
     // Web3 magic here
-    var web3 = new Web3(new Web3.providers.HttpProvider('https://locahost:7545'))
+
+    if (typeof web3 !== 'undefined') {
+        var web3 = new Web3(web3.currentProvider)
+      } else {
+        var web3 = new Web3(new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545')) 
+        //var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/'))
+    }
+
+    //var web3_instance = new Web3(new Web3.providers.HttpProvider('https://locahost:7545'))
     console.log("We have web3 ----- PHEW!!!!");
 
     //const accounts = await web3.eth.getAccounts();
     //console.log("accounts",accounts);
+    
+    const ChainID = await web3.eth.net.getId();
+    console.log("****** CHAIN ID *****",ChainID);
+    var contractAddress;
 
-    console.log("Setting Contract to Local Ganache Address")
-    const contractAddress = "0xF32A0316ABD5E96502c8C8B584D490D690413B7B"; //Ganache - local address 
+    // Ropsten contract address  
+    if (ChainID == 3) {
+        console.log("Setting Contract to Ropsten Testnet Address")
+        contractAddress = "0xa8E381CaB19732aE8aDFE8A94d2dA2b8b5a97c78"; //Ropsten network contract 
+        } else if (ChainID==1337) { 
+         // SET GANACHE CONTRACT ADDRESS HERE
+         console.log("Setting Contract to Local Ganache Address")
+         contractAddress = "0x864e28E80ea58EC20bBD9cbd679dD6268B13649f"; //Ganache - local address 
+        } else {
+         console.log("Network not configured for contract call in Mint.js")
+     }
+    
+
+
+    //console.log("Setting Contract to Local Ganache Address")
+    //const contractAddress = "0x864e28E80ea58EC20bBD9cbd679dD6268B13649f"; //Ganache - local address 
     console.log("CONTRACT ADDRESS GANACHE --> ",contractAddress);
 
-    //const getContract_abi = await fetch('/Users/aheeshnagraj/Documents/GitHub/NodeMessage/step3/app/contracts/compiled/artworkabi.json');
+    
+
+
+    
+
+    //const getContract_abi = await fetch('/Users/aheeshnagraj/Documents/GitHub/NodeMessage/step3/app/contracts/compiled/artworkabi.json', {method: 'POST', body: 'a=1'});
+    //const contract_data = await getContract_abi.json();
+
     const abi_data=fs.readFileSync('/Users/aheeshnagraj/Documents/GitHub/NodeMessage/step3/app/contracts/compiled/artworkabi.json',(err,data)=> {
         if(err) {
             console.error(err);
             return;
         }
         console.log(data);
-    });
+    }); 
+    
     
     const abistring= JSON.parse(abi_data);
     console.log("getContract() typeof abistring --->", typeof(abistring));
-    console.log("getContract() abistring object ---> ",abistring);
+    console.log("getContract() typeof contractAddress --->", typeof(contractAddress));
+    //console.log("getContract() abistring object ---> ",abistring);
+    
+    //console.log("CONTRACT DATA HERE",contract_data);
 
+    //End of reading the contract abi file
+
+    const accounts = await web3.eth.getAccounts();
+    console.log("accounts",accounts)
+    console.log("account ZERO",accounts[0])
+    
+
+    console.log("******* CALLING CONTRACT web3 function******") 
+    const nftContract =   new web3.eth.Contract(abistring,contractAddress);
+
+    console.log("******* did we get nftContract handle******"); 
+
+    const transactionReceipt = await nftContract.methods.registerArtwork(req.body.x,req.body.IpfsHash)
+    .send({
+        from:accounts[0],
+        gas: 6721975, 
+        gasPrice: '30000000'
+    });
+
+    console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
 
 } 
 
